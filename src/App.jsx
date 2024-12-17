b5d399a8094dc653425c10f689031a518dd1a034
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2024-12-17 17:05:04 -0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   return (
     <div>
       <Header />
-      <Post
-        author="Manoel Rodriguez"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente molestias optio laborum 
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Manoel Rodriguez"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente molestias optio laborum 
           tenetur blanditiis inventore cumque quaerat odio suscipit eos esse sunt repellat id impedit, 
           quo nisi doloribus aspernatur expedita."
-      />
+          />
+        </main>
+      </div>
     </div>
   )
 }
