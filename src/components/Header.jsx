c71f05fc39f7f50e896c1a7fcf5569ef6d72aa3b
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2024-12-17 16:24:52 -0300

    Renderizando logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import styles from "./Header.module.css"
+import IgniteLogo from "../assets/ignite-logo.svg"
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
-        </header>
+            <img src={IgniteLogo} alt="Logo do Ignite" />        </header>
     )
 }
