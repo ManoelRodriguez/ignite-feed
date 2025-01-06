3acf25c24fe7cfcb352cdc8ec3fa1b75203b9034
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2025-01-06 16:03:43 -0300

    Estabelecendo comunicação indireta entre Post e Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment(props) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/ManoelRodriguez.png" />
@@ -14,12 +20,12 @@ export function Comment(props) {
                             <time title="18 de dezembro às 15:40" datetime="2024-12-18 15:40:03">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
 
-                    <p>{props.content}</p>
+                    <p>{content}</p>
                 </div>
                 <footer>
                     <button>
